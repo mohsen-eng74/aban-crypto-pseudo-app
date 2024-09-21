3cb15091bb4322c810c5fced355a1661aceac326
mohsen-eng74 <mohsen.eng74@gmail.com>
2024-09-21 17:22:22 +0330

    chore: add `src` directory

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+"""
+Abantether crypto pseudo app
+"""
+
+from __future__ import annotations
+
+__name__ = "aban-crypto-pseudo-app"
+__version__ = "0.1.0"
+__description__ = "Abantether crypto pseudo app"
+__author__ = "Mohsen Rahmati Yami <mohsen.eng74@gmail.com>"
+__license__ = "Proprietary"
+__repository__ = "https://github.com/mohsen-eng74/aban-crypto-pseudo-app"
+__typography__ = rf"""
+   ____ ______   ______ _____ ___          _    ____  ____  
+  / ___|  _ \ \ / /  _ \_   _/ _ \        / \  |  _ \|  _ \ 
+ | |   | |_) \ V /| |_) || || | | |_____ / _ \ | |_) | |_) |
+ | |___|  _ < | | |  __/ | || |_| |_____/ ___ \|  __/|  __/ 
+  \____|_| \_\|_| |_|    |_| \___/     /_/   \_\_|   |_|  v{__version__} 
+"""  # noqa
