bf4f523abcec277fc9de7fffa05bd48fa877a669
mohsen-eng74 <mohsen.eng74@gmail.com>
2024-09-21 17:26:57 +0330

    feat: add project `constant` module

---
+++ src/constant.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from types import MappingProxyType
+
+CURRENCIES_PRICE_MAPPING = MappingProxyType(
+    {
+        "ABAN": 1  # USD
+    }
+)
+MINIMUM_ALLOWED_EXCHANGE_VALUE = 10  # USD
